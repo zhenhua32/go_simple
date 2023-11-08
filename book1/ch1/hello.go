d477b7325648eb133bc4bd5fec6dbacdf0cf3e4b
zhenhua32 <zhenhua32@gmail.com>
2023-11-08 22:38:02 +0800

    重新起飞

---
+++ book1/ch1/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+
+// 运行命令 go run hello.go
+// 编译命令 go build hello.go
+// 安装go工具 go install github.com/rakyll/hey@latest
+// 使用 hey 工具 hey http://www.baidu.com
