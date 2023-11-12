62aa9c8b2dade367b2b1678913bd7e3e26e596d8
zhenhua32 <zhenhua32@gmail.com>
2023-11-12 17:19:42 +0800

    save

--- book1/ch9/go.mod
+++ book1/ch9/go.mod
@@ -1,3 +1,5 @@
 module tzh.com/example
 
 go 1.21.4
+
+require github.com/shopspring/decimal v1.3.1
--- book1/ch9/main.go
+++ book1/ch9/main.go
@@ -5,10 +5,18 @@ import (
 
 	print "tzh.com/example/formatter"
 	"tzh.com/example/math"
+
+	"github.com/shopspring/decimal"
 )
 
 func main() {
 	num := math.Double(2)
 	output := print.Format(num)
 	fmt.Println(output)
+
+	amount, err := decimal.NewFromString("136.02")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(amount)
 }
