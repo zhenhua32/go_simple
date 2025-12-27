b2d7919b380345786563c2d4814cfb1198b528a0
zhenhua32 <zhenhua32@gmail.com>
2025-12-27 10:03:44 +0800

    save

--- book2/ch2/cmdargs/cmdargs.go
+++ book2/ch2/cmdargs/cmdargs.go
@@ -31,11 +31,33 @@ type MenuConf struct {
 	Goodbye bool
 }
 
-func (mc *MenuConf) SetupMenu() *flag.FlagSet {
+func (m *MenuConf) SetupMenu() *flag.FlagSet {
 	menu := flag.NewFlagSet("menu", flag.ExitOnError)
 	menu.Usage = func() {
-		fmt.Printf(greetUsage, os.Args[0])
+		fmt.Printf(usage, os.Args[0])
 		menu.PrintDefaults()
 	}
 	return menu
+}
+
+func (m *MenuConf) GetSubmenu() *flag.FlagSet {
+	submenu := flag.NewFlagSet("submenu", flag.ExitOnError)
+	submenu.BoolVar(&m.Goodbye, "goodbye", false, "say goodbye instead of hello")
+	submenu.Usage = func() {
+		fmt.Printf(greetUsage, os.Args[0])
+		submenu.PrintDefaults()
+	}
+	return submenu
+}
+
+func (m *MenuConf) Greet(name string) {
+	if m.Goodbye {
+		fmt.Println("Goodbye" + name + "!")
+	} else {
+		fmt.Println("Hello" + name + "!")
+	}
+}
+
+func (m *MenuConf) Version() {
+	fmt.Println("Version:" + version)
 }
