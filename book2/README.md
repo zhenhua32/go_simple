ba19e04b678b7d6f8a1ce2322ab72d8cd3fe9840
zhenhua32 <zhenhua32@gmail.com>
2025-12-28 14:20:35 +0800

    save

--- book2/README.md
+++ book2/README.md
@@ -3,5 +3,5 @@
 https://github.com/PacktPublishing/Go-Programming-Cookbook-Second-Edition/tree/master
 
 ```bash
-go mod init book2/ch2/pipes
+go mod init book2/ch2/signals
 ```
---
+++ book2/ch2/signals/signals.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func CatchSig(ch chan os.Signal, done chan bool) {
+	sig := <-ch
+	fmt.Println("nsig received:", sig)
+
+	switch sig {
+	case syscall.SIGINT:
+		fmt.Println("handing a SIGINT now")
+	case syscall.SIGTERM:
+		fmt.Println("handing a SIGTERM now")
+	default:
+		fmt.Println("unknown signal:", sig)
+	}
+
+	done <- true
+}
+
+func main() {
+	signals := make(chan os.Signal)
+	done := make(chan bool)
+
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go CatchSig(signals, done)
+	fmt.Println("press ctrl+c to terminate...")
+	<-done
+	fmt.Println("Done!")
+}
