9382239d69eeef21c55f9602a53df7c710d7f19f
zhenhua32 <zhenhua32@gmail.com>
2026-01-03 21:41:11 +0800

    save

--- book2/README.md
+++ book2/README.md
@@ -3,5 +3,5 @@
 https://github.com/PacktPublishing/Go-Programming-Cookbook-Second-Edition/tree/master
 
 ```bash
-go mod init book2/ch3/nulls
+go mod init book2/ch3/encoding
 ```
---
+++ book2/ch3/encoding/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "book2/ch3/encoding"
+
+func main() {
+	if err := encoding.Base64Example(); err != nil {
+		panic(err)
+	}
+
+	if err := encoding.Base64ExampleEncoder(); err != nil {
+		panic(err)
+	}
+
+	if err := encoding.GobExample(); err != nil {
+		panic(err)
+	}
+}
